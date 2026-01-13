2f48abafa4da49173c989de00764e125a1f97bba
AW-Learning <learning@alexwhite.digital>
2026-01-13 15:44:22 -0600

    add controls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,15 +9,23 @@ def main():
     print("Starting Asteroids")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT /2
+    player = Player(x,y)
+    clock = pygame.time.Clock()
 
     while True:
         log_state()
+        dt_ms = clock.tick(60)
+        dt = dt_ms / 1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
 
